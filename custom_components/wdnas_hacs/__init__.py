b52892159663c272ffc85c9e3d0c09f4338da292
Josh <91017208+J-shw@users.noreply.github.com>
2024-10-20 22:49:59 +0100

    Update __init__.py

--- custom_components/wdnas_hacs/__init__.py
+++ custom_components/wdnas_hacs/__init__.py
@@ -1,69 +1,28 @@
-"""WD NAS cient hacs integration"""
-
-from __future__ import annotations
-
-from typing import TYPE_CHECKING
-
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.loader import async_get_loaded_integration
+from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD, Platform
 
-from .api import IntegrationBlueprintApiClient
-from .coordinator import BlueprintDataUpdateCoordinator
-from .data import IntegrationBlueprintData
+from py2n import Py2NDevice, Py2NConnectionData
 
-if TYPE_CHECKING:
-    from homeassistant.core import HomeAssistant
+from .const import DOMAIN
+from .coordinator import Helios2nPortDataUpdateCoordinator, Helios2nSwitchDataUpdateCoordinator
 
-    from .data import IntegrationBlueprintConfigEntry
+platforms = [Platform.BUTTON, Platform.LOCK, Platform.SWITCH, Platform.BINARY_SENSOR]
 
-PLATFORMS: list[Platform] = [
-    Platform.SENSOR,
-    Platform.BINARY_SENSOR,
-    Platform.SWITCH,
-]
-
-
-# https://developers.home-assistant.io/docs/config_entries_index/#setting-up-an-entry
-async def async_setup_entry(
-    hass: HomeAssistant,
-    entry: IntegrationBlueprintConfigEntry,
-) -> bool:
-    """Set up this integration using UI."""
-    coordinator = BlueprintDataUpdateCoordinator(
-        hass=hass,
-    )
-    entry.runtime_data = IntegrationBlueprintData(
-        client=IntegrationBlueprintApiClient(
-            username=entry.data[CONF_USERNAME],
-            password=entry.data[CONF_PASSWORD],
-            session=async_get_clientsession(hass),
-        ),
-        integration=async_get_loaded_integration(hass, entry.domain),
-        coordinator=coordinator,
-    )
-
-    # https://developers.home-assistant.io/docs/integration_fetching_data#coordinated-single-api-poll-for-data-for-all-entities
-    await coordinator.async_config_entry_first_refresh()
-
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
-
-    return True
-
-
-async def async_unload_entry(
-    hass: HomeAssistant,
-    entry: IntegrationBlueprintConfigEntry,
-) -> bool:
-    """Handle removal of an entry."""
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-
-
-async def async_reload_entry(
-    hass: HomeAssistant,
-    entry: IntegrationBlueprintConfigEntry,
-) -> None:
-    """Reload config entry."""
-    await async_unload_entry(hass, entry)
-    await async_setup_entry(hass, entry)
+async def async_setup_entry(hass: HomeAssistant, config: ConfigType) -> bool:
+	aiohttp_session = async_get_clientsession(hass)
+	connection_data = Py2NConnectionData(host= config.data[CONF_HOST], username=config.data[CONF_USERNAME], password=config.data[CONF_PASSWORD])
+	device = await Py2NDevice.create(aiohttp_session, connection_data)
+	hass.data.setdefault(DOMAIN,{})[config.entry_id] = device
+	for platform in platforms:
+		hass.data[DOMAIN].setdefault(platform, {})
+	hass.data[DOMAIN][Platform.LOCK]["coordinator"] = Helios2nSwitchDataUpdateCoordinator(hass, device)
+	hass.data[DOMAIN][Platform.SWITCH]["coordinator"] = Helios2nPortDataUpdateCoordinator(hass, device)
+	hass.data[DOMAIN][Platform.BINARY_SENSOR]["coordinator"] = Helios2nPortDataUpdateCoordinator(hass, device)
+	hass.async_create_task(
+		hass.config_entries.async_forward_entry_setups(
+		config, platforms
+		)
+	)
+	return True
